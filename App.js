0c5465198ba1dfeee7a556970c9c4e015bd909a7
Shamshad259 <shamshad259@gmail.com>
2024-07-18 20:00:18 +0530

    2nd

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-// const heading = React.createElement("h1",{id:"heading"},"Hello World from Shamshad Ahammad");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
@@ -16,4 +15,4 @@ const parent = React.createElement("div", { id: "parent" }, [
 const root = ReactDOM.createRoot(document.getElementById("root"));
 setTimeout(() => {
   root.render(parent);
-}, 2000);
+}, 1000);
