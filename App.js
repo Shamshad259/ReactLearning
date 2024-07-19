bda913d37cd5f4ea45a1057146069071da198ff4
Shamshad259 <shamshad259@gmail.com>
2024-07-19 18:15:56 +0530

    3rd

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "This is h1"),
-    React.createElement("h2", {}, "This is h2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "This is h1 inside child2"),
-    React.createElement("h2", {}, "This is h2 inside child2"),
-  ]),
-]);
+// React.createElement =>  Object => HTMLElement(render)
+
+const heading = React.createElement("h1",{id:"heading"},"This is React's Magic!!!");
+
+// JSX - HTML like syntax - Not HTML or Not part of react
+// JSX transpiles to React.createElement => React Element => JS Object
+
+
+const jsxHeading = <h1 id="headings">This JSX React!!!</h1>;
+
+// Functional Component
+
+
+const Heading2 = ()=>{
+    return <h2 id="heading2">This is inside... Component Composition</h2>
+}
+
+
+const Heading = ()=>{
+    return (<div id="container">
+        <h1 id="heading">This is Functional Component</h1>
+        <Heading2/>
+        {Heading2()}
+        </div>)
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-setTimeout(() => {
-  root.render(parent);
-}, 1000);
+root.render(<Heading/>);
+
