8c4dd0dc694fa02d979aaf938913773a90cc9e92
Shamshad259 <shamshad259@gmail.com>
2024-07-21 07:53:08 +0530

    6th

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,32 +1,66 @@
 import restaurants from "../utils/mockdata";
 import ResCard from "./ResCard";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [res, setRes] = useState(restaurants);
-    return (
-      <div className="body">
-        {/* <div className="search-container">Search</div> */}
-        <div className="filter-res">
-            <button className="filter-btn"  onClick={
-            () => {
-                const filteredRes = res.filter((restaurant)=>
-                restaurant.info.avgRating>4.5);
-                setRes(filteredRes);
-            }
-        }>
-            Top Rated Restaurants
-            </button>
-            </div>
+  const [res, setRes] = useState(restaurants);
+  const [filteredRest,setFilteredRest] = useState(res);
+  const [searchText,setSearchText] = useState("")
 
-        <div className="res-container">
-          {res.map(restaurant=>{
-            return <ResCard key={restaurant.info.id} resData={restaurant} />
-          })}
-        </div>
-      </div>
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/mapi/homepage/getCards?lat=10.0037578&lng=76.3579401"
+    );
+    const json = await data.json();
+    setRes(
+      json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle
+        ?.restaurants
     );
   };
 
-  export default Body;
+  if (res.length === 0) {
+    return <Shimmer />;
+  }
+  return (
+    <div className="body">
+      <div className="filter-res">
+        <div className="search-container">
+        <input type="test" className="search-input" value={searchText} placeholder="Search..." onChange={(e)=>{
+          setSearchText(e.target.value);
+        }}></input>
+        <button onClick={()=>{
+          const filteredRes = res.filter((restaurant)=>
+          restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())
+          )
+          setFilteredRest(filteredRes);
+        }}>Search</button>
+
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRes = res.filter(
+              (restaurant) => restaurant.info.avgRating > 4.5
+            );
+            setRes(filteredRes);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
+      <div className="res-container">
+        {filteredRest.map((restaurant) => {
+          return <ResCard key={restaurant.info.id} resData={restaurant} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
