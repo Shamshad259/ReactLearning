cb01cd167db46ba8815ef9d31b3ef8b300f8e0de
Shamshad259 <shamshad259@gmail.com>
2024-07-24 11:13:14 +0530

    8.2.0

--- src/components/About.js
+++ src/components/About.js
@@ -6,23 +6,15 @@ import {Component} from "react";
 class About extends Component{
     constructor(props){
         super(props);
-        console.log("Parent Constructor");
-    }
-
-    componentDidMount(){
-        console.log("Parent Componet Did Mount");
     }
 
     render(){
-        console.log("Parent Render");
         return(
             <div className="about">
                 <h1>About Us</h1>
                 <h2>This is about us page</h2>
             <User name={"Shamshad Ahammad (function)"} location={"Vadakara (function)"}/>
-            <UserClass name={"First"} location={"Vadakara (class)"}/>
-            <UserClass name={"Second"} location={"Vadakara (class)"}/>
-            <UserClass name={"Third"} location={"Vadakara (class)"}/>
+            <UserClass />
             </div>
         )
     }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,36 +1,57 @@
 import React from "react";
 
-class UserClass extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            count:0
-        }
-        console.log(this.props.name+" Child Constructor")
-    }
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      userInfo: {
+        name: "",
+        location: "",
+      },
+    };
+    console.log("Constructor")
+  }
 
-    componentDidMount(){
-        console.log(this.props.name+" Child Component Did Mount");
-    }
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/Shamshad259");
+    const json = await data.json();
+    this.setState({
+        userInfo:json
+    });
+    console.log("Component Did Mount")
+  }
 
-    render(){
-        console.log(this.props.name+" Child Render");
-        const {name,location} = this.props;
-        const {count} = this.state;
-        return (
-            <div className="user-card">
-                <h5>Count : {count}</h5>
-                <button onClick={()=>{
-                    this.setState({
-                        count:count+1
-                    })
-                }}>Increment Count</button>
-                <h2>{name}</h2>
-                <h2>{location}</h2>
-                <h3>shamshad@gmail.com</h3>
-            </div>
-        )
-    }
+  componentDidUpdate(){
+    console.log("Updated");
+  }
+
+  componentWillUnmount(){
+    console.log("Component will unmount");
+  }
+
+  render() {
+    console.log("Child Render");
+    const { name, location } = this.state.userInfo;
+    const { count } = this.state;
+    return (
+      <div className="user-card">
+        <h5>Count : {count}</h5>
+        <button
+          onClick={() => {
+            this.setState({
+              count: count + 1,
+            });
+          }}
+        >
+          Increment Count
+        </button>
+        <h2>{name}</h2>
+        <h2>{location}</h2>
+        <h3>shamshad@gmail.com</h3>
+      </div>
+    );
+  }
 }
 
 export default UserClass;
